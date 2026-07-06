137f1a6ebea85b337beb57dd2f44a6ad3965c121
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-06 13:24:33 +0700

    Last Sync: 2026-07-06 13:24 (Mobile)

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 URLMANGA = [""] 
-URLCHAPTER = ["https://vymanga.net/read/checkmate-r/2247127"] 
+URLCHAPTER = ["https://vymanga.net/read/checkmate-r/2247124"] 
 
 FONT_PATH = "Roboto-Bold.ttf"
