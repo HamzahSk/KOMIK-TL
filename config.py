d36e1827eabca44bf89bc5751434723aa699fc0a
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-08 15:47:50 +0700

    Last Sync: 2026-07-08 15:47 (Mobile)

--- config.py
+++ config.py
@@ -1,20 +1,6 @@
 # config.py
-URLMANGA = [""] 
-URLCHAPTER = [
-  "https://vymanga.net/read/checkmate-r/2247294",
-  "https://vymanga.net/read/checkmate-r/2247296",
-  "https://vymanga.net/read/checkmate-r/3142526",
-  "https://vymanga.net/read/checkmate-r/2247299",
-  "https://vymanga.net/read/checkmate-r/2247301",
-  "https://vymanga.net/read/checkmate-r/2247303",
-  "https://vymanga.net/read/checkmate-r/2247306",
-  "https://vymanga.net/read/checkmate-r/2247308",
-  "https://vymanga.net/read/checkmate-r/2247310",
-  "https://vymanga.net/read/checkmate-r/2247313",
-  "https://vymanga.net/read/checkmate-r/2247314",
-  "https://vymanga.net/read/checkmate-r/2247318",
-  "https://vymanga.net/read/checkmate-r/2247322",
-  "https://vymanga.net/read/checkmate-r/2247328"
-]
+URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
+URLCHAPTER = ["https://bbato.com/read/soulbound/chapter-0"]
+# https://vymanga.com/manga/reverse-thinking
 
-FONT_PATH = "Roboto-Bold.ttf"
+FONT_PATH = "digistrip.ttf"
