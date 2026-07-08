2051f1407b9dabcdbf04d53bc2329e00fa4d2843
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-08 16:19:38 +0700

    Last Sync: 2026-07-08 16:19 (Mobile)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
-URLCHAPTER = ["https://bbato.com/read/soulbound/chapter-0"]
+URLMANGA = ["https://vymanga.com/read/reverse-thinking/2853358"] # https://bbato.com/manga/soulbound"] 
+URLCHAPTER = [""] #["https://bbato.com/read/soulbound/chapter-0"]
 # https://vymanga.com/manga/reverse-thinking
 
 FONT_PATH = "digistrip.ttf"
