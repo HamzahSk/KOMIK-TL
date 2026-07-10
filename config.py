ad3cb2d7e775a66fa6c5637b67702d61281a11b1
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-10 19:14:11 +0700

    Last Sync: 2026-07-10 19:14 (Mobile)

--- config.py
+++ config.py
@@ -1,52 +1,8 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    "https://vymanga.com/read/turning/2301737",  # Chapter 20
-    "https://vymanga.com/read/turning/2307297",  # Chapter 21
-    "https://vymanga.com/read/turning/2313316",  # Chapter 22
-    "https://vymanga.com/read/turning/2319516",  # Chapter 23
-    "https://vymanga.com/read/turning/2330257",  # Chapter 24
-    "https://vymanga.com/read/turning/2336433",  # Chapter 25
-    "https://vymanga.com/read/turning/2341748",  # Chapter 26
-    "https://vymanga.com/read/turning/2348643",  # Chapter 27
-    "https://vymanga.com/read/turning/2351164",  # Chapter 27.5
-    "https://vymanga.com/read/turning/2364291",  # Chapter 28
-    "https://vymanga.com/read/turning/2371374",  # Chapter 29
-    "https://vymanga.com/read/turning/2375826",  # Chapter 30
-    "https://vymanga.com/read/turning/2404690",  # Chapter 31
-    "https://vymanga.com/read/turning/2420689",  # Chapter 32
-    "https://vymanga.com/read/turning/2432107",  # Chapter 33
-    "https://vymanga.com/read/turning/2475325",  # Chapter 34
-    "https://vymanga.com/read/turning/2528400",  # Chapter 35
-    "https://vymanga.com/read/turning/2530987",  # Chapter 36
-    "https://vymanga.com/read/turning/2549085",  # Chapter 37
-    "https://vymanga.com/read/turning/2610027",  # Chapter 38
-    "https://vymanga.com/read/turning/2616622",  # Chapter 39
-    "https://vymanga.com/read/turning/2623418",  # Chapter 40
-    "https://vymanga.com/read/turning/2629190",  # Chapter 41
-    "https://vymanga.com/read/turning/2635111",  # Chapter 42
-    "https://vymanga.com/read/turning/2642297",  # Chapter 43
-    "https://vymanga.com/read/turning/2649912",  # Chapter 44
-    "https://vymanga.com/read/turning/2658921",  # Chapter 45
-    "https://vymanga.com/read/turning/2667759",  # Chapter 46
-    "https://vymanga.com/read/turning/2675013",  # Chapter 47
-    "https://vymanga.com/read/turning/2682286",  # Chapter 48
-    "https://vymanga.com/read/turning/2691548",  # Chapter 49
-    "https://vymanga.com/read/turning/2699015",  # Chapter 50
-    "https://vymanga.com/read/turning/2705631",  # Chapter 51
-    "https://vymanga.com/read/turning/2712028",  # Chapter 52
-    "https://vymanga.com/read/turning/2724917",  # Chapter 53
-    "https://vymanga.com/read/turning/2724918",  # Chapter 54
-    "https://vymanga.com/read/turning/2732077",  # Chapter 55
-    "https://vymanga.com/read/turning/2738611",  # Chapter 56
-    "https://vymanga.com/read/turning/2745191",  # Chapter 57
-    "https://vymanga.com/read/turning/2751127",  # Chapter 58
-    "https://vymanga.com/read/turning/2758360",  # Chapter 59
-    "https://vymanga.com/read/turning/2763678",  # Chapter 60
-    "https://vymanga.com/read/turning/2769933",  # Chapter 61
-    "https://vymanga.com/read/turning/2778538",  # Chapter 62
-    "https://vymanga.com/read/turning/2785049",  # Chapter 63
-    "https://vymanga.com/read/turning/2791526",  # Chapter 64
+    "https://vymanga.com/read/bad-friend-euneun/2990659",
+    "https://vymanga.com/read/bad-friend-euneun/2999918"
 ]
 
 
