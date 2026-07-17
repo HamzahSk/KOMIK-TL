be748fdd41353df49d189b0420300933a76bd35c
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-17 14:09:31 +0700

    Last Sync: 2026-07-17 14:09 (Mobile)

--- config.py
+++ config.py
@@ -1,25 +1,33 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    "https://vymanga.com/read/senza-replica/2405474",
-    "https://vymanga.com/read/senza-replica/2412182",
-    "https://vymanga.com/read/senza-replica/2429533",
-    "https://vymanga.com/read/senza-replica/2442704",
-    "https://vymanga.com/read/senza-replica/2442706",
-    "https://vymanga.com/read/senza-replica/2451515",
-    "https://vymanga.com/read/senza-replica/2459679",
-    "https://vymanga.com/read/senza-replica/2466899",
-    "https://vymanga.com/read/senza-replica/2474144",
-    "https://vymanga.com/read/senza-replica/2485260",
-    "https://vymanga.com/read/senza-replica/2488050",
-    "https://vymanga.com/read/senza-replica/2496498",
-    "https://vymanga.com/read/senza-replica/2505201",
-    "https://vymanga.com/read/senza-replica/2513857",
-    "https://vymanga.com/read/senza-replica/2513773",
-    "https://vymanga.com/read/senza-replica/2525756",
-    "https://vymanga.com/read/senza-replica/2534322",
-    "https://vymanga.com/read/senza-replica/2532333",
-    "https://vymanga.com/read/senza-replica/2538243",
+    "https://vymanga.com/read/senza-replica/2627447",  # Chapter 35
+    "https://vymanga.com/read/senza-replica/2632988",  # Chapter 36
+    "https://vymanga.com/read/senza-replica/2639957",  # Chapter 37
+    "https://vymanga.com/read/senza-replica/2647757",  # Chapter 38
+    "https://vymanga.com/read/senza-replica/2656388",  # Chapter 39
+    "https://vymanga.com/read/senza-replica/2665788",  # Chapter 40
+    "https://vymanga.com/read/senza-replica/2672930",  # Chapter 41
+    "https://vymanga.com/read/senza-replica/2680210",  # Chapter 42
+    "https://vymanga.com/read/senza-replica/2688300",  # Chapter 43
+    "https://vymanga.com/read/senza-replica/2697192",  # Chapter 44
+    "https://vymanga.com/read/senza-replica/2704392",  # Chapter 45
+    "https://vymanga.com/read/senza-replica/2710621",  # Chapter 46
+    "https://vymanga.com/read/senza-replica/2863938",  # Chapter 47
+    "https://vymanga.com/read/senza-replica/2748971",  # Chapter 48
+    "https://vymanga.com/read/senza-replica/2754887",  # Chapter 49
+    "https://vymanga.com/read/senza-replica/2762074",  # Chapter 50
+    "https://vymanga.com/read/senza-replica/2782184",  # Chapter 51
+    "https://vymanga.com/read/senza-replica/2784040",  # Chapter 52
+    "https://vymanga.com/read/senza-replica/2800282",  # Chapter 53
+    "https://vymanga.com/read/senza-replica/2801788",  # Chapter 54
+    "https://vymanga.com/read/senza-replica/2803713",  # Chapter 55
+    "https://vymanga.com/read/senza-replica/2810844",  # Chapter 56
+    "https://vymanga.com/read/senza-replica/2811714",  # Chapter 57
+    "https://vymanga.com/read/senza-replica/2818758",  # Chapter 58
+    "https://vymanga.com/read/senza-replica/2823890",  # Chapter 59
+    "https://vymanga.com/read/senza-replica/2831144",  # Chapter 60
+    "https://vymanga.com/read/senza-replica/2837396",  # Chapter 61
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
 # https://vymanga.com/manga/reverse-thinking
