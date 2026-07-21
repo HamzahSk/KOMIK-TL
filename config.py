7d33437d0f69d37ffeaea0709cecf7a57ea29f3d
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-21 19:13:24 +0700

    Last Sync: 2026-07-21 19:13 (Mobile)

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-   # "https://vymanga.com/read/lookalike/2199097", 
+   # "https://vymanga.com/read/lookalike/2199097",
     "https://vymanga.com/read/senza-replica/2823890"  # Chapter 36
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
