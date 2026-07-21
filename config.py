957fdf628d1ed51a03e2746c6c3452bb9c9722a1
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-21 23:21:34 +0700

    Last Sync: 2026-07-21 23:21 (Mobile)

--- config.py
+++ config.py
@@ -1,8 +1,40 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    #"https://vymanga.com/read/eighteens-bed/2832843",
-    "https://vymanga.com/read/senza-replica/2823890"  # Chapter 36
+    "https://vymanga.com/read/lookalike/2199097",  # Chapter 36
+    "https://vymanga.com/read/lookalike/2204515",  # Chapter 37
+    "https://vymanga.com/read/lookalike/2209906",  # Chapter 38
+    "https://vymanga.com/read/lookalike/2217314",  # Chapter 39
+    "https://vymanga.com/read/lookalike/2227603",  # Chapter 40
+    "https://vymanga.com/read/lookalike/2233355",  # Chapter 41
+    "https://vymanga.com/read/lookalike/2238872",  # Chapter 42
+    "https://vymanga.com/read/lookalike/2245488",  # Chapter 43
+    "https://vymanga.com/read/lookalike/2256919",  # Chapter 44
+    "https://vymanga.com/read/lookalike/2262240",  # Chapter 45
+    "https://vymanga.com/read/lookalike/2269504",  # Chapter 46
+    "https://vymanga.com/read/lookalike/2274529",  # Chapter 47
+    "https://vymanga.com/read/lookalike/2285396",  # Chapter 48
+    "https://vymanga.com/read/lookalike/2292141",  # Chapter 49
+    "https://vymanga.com/read/lookalike/2298520",  # Chapter 50
+    "https://vymanga.com/read/lookalike/2304101",  # Chapter 51
+    "https://vymanga.com/read/lookalike/2316406",  # Chapter 52
+    "https://vymanga.com/read/lookalike/2322169",  # Chapter 53
+    "https://vymanga.com/read/lookalike/2328504",  # Chapter 54
+    "https://vymanga.com/read/lookalike/2334240",  # Chapter 55
+    "https://vymanga.com/read/lookalike/2344475",  # Chapter 56
+    "https://vymanga.com/read/lookalike/2350224",  # Chapter 57
+    "https://vymanga.com/read/lookalike/2355728",  # Chapter 58
+    "https://vymanga.com/read/lookalike/2361476",  # Chapter 59
+    "https://vymanga.com/read/lookalike/2367593",  # Chapter 60
+    "https://vymanga.com/read/lookalike/2376343",  # Chapter 0.1
+    "https://vymanga.com/read/lookalike/2376344",  # Chapter 0.2
+    "https://vymanga.com/read/lookalike/2380065",  # Chapter 0.3
+    "https://vymanga.com/read/lookalike/2385774",  # Chapter 0.4
+    "https://vymanga.com/read/lookalike/2392285",  # Chapter 0.5
+    "https://vymanga.com/read/lookalike/2395764",  # Chapter 0.6
+    "https://vymanga.com/read/lookalike/2400940",  # Chapter 0.7
+    "https://vymanga.com/read/lookalike/2406599",  # Chapter 0.8
+    "https://vymanga.com/read/lookalike/2411055",  # Chapter 0.9
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
 # https://vymanga.com/manga/reverse-thinking
