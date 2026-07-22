7d1423e566b9af18b3dffd93c5e7cf4606b5876a
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-22 20:51:00 +0700

    Last Sync: 2026-07-22 20:51 (Mobile)

--- config.py
+++ config.py
@@ -1,9 +1,53 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    "https://vymanga.com/read/reverse-thinking/2570728",  # Chapter 1
-    "https://vymanga.com/read/reverse-thinking/2573222",  # Chapter 2
-    "https://vymanga.com/read/reverse-thinking/2573380",  # Chapter 3
+    "https://vymanga.com/read/reverse-thinking/2622560",
+    "https://vymanga.com/read/reverse-thinking/2632227",
+    "https://vymanga.com/read/reverse-thinking/2665903",  # 11.5
+    "https://vymanga.com/read/reverse-thinking/2642010",
+    "https://vymanga.com/read/reverse-thinking/2665904",  # 12.5
+    "https://vymanga.com/read/reverse-thinking/2652937",
+    "https://vymanga.com/read/reverse-thinking/2684851",
+    "https://vymanga.com/read/reverse-thinking/2690608",  # 14.5
+    "https://vymanga.com/read/reverse-thinking/2698384",
+    "https://vymanga.com/read/reverse-thinking/2708167",
+    "https://vymanga.com/read/reverse-thinking/2719318",
+    "https://vymanga.com/read/reverse-thinking/2729017",
+    "https://vymanga.com/read/reverse-thinking/2738415",
+    "https://vymanga.com/read/reverse-thinking/2748712",
+    "https://vymanga.com/read/reverse-thinking/2766235",
+    "https://vymanga.com/read/reverse-thinking/2778187",
+    "https://vymanga.com/read/reverse-thinking/2799185",
+    "https://vymanga.com/read/reverse-thinking/2809013",
+    "https://vymanga.com/read/reverse-thinking/2821079",
+    "https://vymanga.com/read/reverse-thinking/2835576",
+    "https://vymanga.com/read/reverse-thinking/2846558",
+    "https://vymanga.com/read/reverse-thinking/3095859",  # 27.1
+    "https://vymanga.com/read/reverse-thinking/3119161",  # 27.2
+    "https://vymanga.com/read/reverse-thinking/3160112",  # 27.3
+    "https://vymanga.com/read/reverse-thinking/3269219",  # 27.4
+    "https://vymanga.com/read/reverse-thinking/3269221",  # 27.5
+    "https://vymanga.com/read/reverse-thinking/3269224",  # 27.6
+    "https://vymanga.com/read/reverse-thinking/3269226",  # 27.7
+    "https://vymanga.com/read/reverse-thinking/3269229",  # 27.8
+    "https://vymanga.com/read/reverse-thinking/3269232",  # 27.9
+    "https://vymanga.com/read/reverse-thinking/2853358",
+    "https://vymanga.com/read/reverse-thinking/2873456",
+    "https://vymanga.com/read/reverse-thinking/2885996",
+    "https://vymanga.com/read/reverse-thinking/2894781",
+    "https://vymanga.com/read/reverse-thinking/2914216",
+    "https://vymanga.com/read/reverse-thinking/2920845",
+    "https://vymanga.com/read/reverse-thinking/2928102",
+    "https://vymanga.com/read/reverse-thinking/3042913",
+    "https://vymanga.com/read/reverse-thinking/3075884",
+    "https://vymanga.com/read/reverse-thinking/3095860",
+    "https://vymanga.com/read/reverse-thinking/3118857",
+    "https://vymanga.com/read/reverse-thinking/3140973",
+    "https://vymanga.com/read/reverse-thinking/3159922",
+    "https://vymanga.com/read/reverse-thinking/3201213",
+    "https://vymanga.com/read/reverse-thinking/3218693",
+    "https://vymanga.com/read/reverse-thinking/3240454",
+    "https://vymanga.com/read/reverse-thinking/3268268",
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
 # https://vymanga.com/manga/reverse-thinking
