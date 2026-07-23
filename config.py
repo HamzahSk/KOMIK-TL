c0c1f2b2420000d4dc393757d3fff019c5c9ae86
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-23 21:20:13 +0700

    Last Sync: 2026-07-23 21:20 (Mobile)

--- config.py
+++ config.py
@@ -1,26 +1,7 @@
 # config.py
-URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
-URLCHAPTER = chapter_urls = [
-    "https://vymanga.com/read/non-zero-sum/2088875",  # Chapter 1
-    "https://vymanga.com/read/non-zero-sum/2089059",  # Chapter 2
-    "https://vymanga.com/read/non-zero-sum/2090642",  # Chapter 3
-    "https://vymanga.com/read/non-zero-sum/2091491",  # Chapter 4
-    "https://vymanga.com/read/non-zero-sum/2093090",  # Chapter 5
-    "https://vymanga.com/read/non-zero-sum/2098282",  # Chapter 6
-    "https://vymanga.com/read/non-zero-sum/2099929",  # Chapter 7
-    "https://vymanga.com/read/non-zero-sum/2100841",  # Chapter 8
-    "https://vymanga.com/read/non-zero-sum/2104030",  # Chapter 9
-    "https://vymanga.com/read/non-zero-sum/2112343",  # Chapter 10
-    "https://vymanga.com/read/non-zero-sum/2121880",  # Chapter 11
-    "https://vymanga.com/read/non-zero-sum/2129657",  # Chapter 12
-    "https://vymanga.com/read/non-zero-sum/2132284",  # Chapter 13
-    "https://vymanga.com/read/non-zero-sum/2138571",  # Chapter 14
-    "https://vymanga.com/read/non-zero-sum/2145408",  # Chapter 15
-    "https://vymanga.com/read/non-zero-sum/2150076",  # Chapter 16
-    "https://vymanga.com/read/non-zero-sum/2155111",  # Chapter 17
-    "https://vymanga.com/read/non-zero-sum/2160457",  # Chapter 18
-    "https://vymanga.com/read/non-zero-sum/2166178",  # Chapter 19
-    "https://vymanga.com/read/non-zero-sum/2171220",  # Chapter 20
+URLMANGA = ["https://vymanga.com/manga/the-horizon"] # https://bbato.com/manga/soulbound"] 
+URLCHAPTER = [
+    "",
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
 # https://vymanga.com/manga/reverse-thinking
