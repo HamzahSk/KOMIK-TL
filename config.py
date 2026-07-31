90b1542b671ea00356c9ca384f866de019052f17
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-31 11:12:29 +0700

    Last Sync: 2026-07-31 11:12 (Mobile)

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
+URLMANGA = ["https://vymanga.com/manga/omniscient-readers-viewpoint-blind-doujinshi", "https://vymanga.com/manga/after-a-meal"] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    "https://vymanga.com/read/reverse-thinking/2853358",
+    "",
 
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
