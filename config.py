e44e44b5d11de969f0dcc062002bc3c6a57bc7ef
Hamzah <hamzahhaz740@gmail.com>
2026-08-24 19:22:43 +0700

    Last Sync: 2026-08-24 19:22 (Mobile)

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 # config.py
 URLMANGA = [""] # https://bbato.com/manga/soulbound"] 
 URLCHAPTER = [
-    "https://kaliscan.com/manga/49669-perle/chapter-17",
-    "https://kaliscan.com/manga/49669-perle/chapter-18",
-    "https://kaliscan.com/manga/49669-perle/chapter-19",
-    "https://kaliscan.com/manga/49669-perle/chapter-20",
-    "https://kaliscan.com/manga/49669-perle/chapter-21",
-    "https://kaliscan.com/manga/49669-perle/chapter-22",
-    "https://kaliscan.com/manga/49669-perle/chapter-23",
-    "https://kaliscan.com/manga/49669-perle/chapter-24",
-    "https://kaliscan.com/manga/49669-perle/chapter-25",
-    "https://kaliscan.com/manga/49669-perle/chapter-26",
-    "https://kaliscan.com/manga/49669-perle/chapter-27",
-    "https://kaliscan.com/manga/49669-perle/chapter-28",
-    "https://kaliscan.com/manga/49669-perle/chapter-29",
-    "https://kaliscan.com/manga/49669-perle/chapter-30",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-0",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-1",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-2",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-3",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-4",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-5",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-6",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-7",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-8",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-9",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-10",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-12",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-13",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-14",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-15",
+    "https://kaliscan.com/manga/26629-the-constellation-of-us/chapter-16",
 ]
  # ["https://vymanga.com/read/reverse-thinking/2853358"]  ["https://bbato.com/read/soulbound/chapter-0"] 
 # https://vymanga.com/manga/reverse-thinking  https://kaliscan.com/manga/3172-radio-storm/chapter-2
